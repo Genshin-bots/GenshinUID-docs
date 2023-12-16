638e963591721929775e7fb4104cbb0a181a4b09
KimigaiiWuyi <KimigaiiWuyi@users.noreply.github.com>
2023-12-16 21:00:38 +0000

    📝 新增`MD模板和按钮模板`文档 d16ebe170cd5ac7fa47e880477af19cd939b319e

---
+++ assets/chunks/Badge.167f4610.js
@@ -0,0 +1,1 @@
+import{d as t,o as a,c as s,r as o,n as _,a as n,t as p,_ as r}from"./framework.d8868ba7.js";const c=t({__name:"Badge",props:{text:{},type:{}},setup(d){return(e,l)=>(a(),s("span",{class:_(["VPBadge",e.type??"tip"])},[o(e.$slots,"default",{},()=>[n(p(e.text),1)],!0)],2))}});const u=r(c,[["__scopeId","data-v-d01e489a"]]);export{u as _};
